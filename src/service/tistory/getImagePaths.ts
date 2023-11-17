3e078f0f0769c8064fdc61469b33cb3dd34714ba
jojoldu <jojoldu@gmail.com>
2023-11-18 01:34:25 +0900

    불필요한 코드 리팩토링

--- src/service/tistory/getImagePaths.ts
+++ src/service/tistory/getImagePaths.ts
@@ -1,5 +1,5 @@
 export function getImagePaths(mdContent: string): string[] {
-  return (mdContent.match(/(?:!\[(.*?)\]\((.*?)\))/g) || []).map((content) =>
+  return (mdContent.match(/!\[(.*?)]\((.*?)\)/g) ?? []).map((content) =>
     content.split('(')[1].replace(')', ''),
   );
 }
